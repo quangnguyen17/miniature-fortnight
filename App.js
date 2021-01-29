1f9ff5b89cadf76fe85c92d38553dab0f52639d3
Vicky Banh <gvbanh@gmail.com>
2021-01-29 16:46:25 -0500

    first init

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, FlatList, View, Text } from 'react-native';
+import mockData from './MOCK_DATA.json';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [users] = useState(mockData);
+	return (
+		<View style={styles.container}>
+			<Text>Hi</Text>
+			<FlatList
+				data={users}
+				keyExtractor={(item) => `${item.id}`}
+				renderItem={({ item }) => (
+					<View style={styles.item}>
+						<Text>{`${item.firstName} ${item.lastName} - ${item.email}`}</Text>
+					</View>
+				)}
+				style={styles.list}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	list: {
+		flex: 1,
+	},
+	item: {
+		padding: 4,
+		marginBottom: 4,
+	},
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					root: '.',
+					alias: {
+						'@assets': './assets',
+						'@actions': './actions',
+						'@api': './api',
+						'@theme': './theme',
+						'@components': './components',
+						'@const': './const',
+						'@helpers': './helpers',
+						'@reducers': './reducers',
+						'@screens': './screens',
+						'@middleware': './middleware',
+						'@interceptors': './interceptors',
+						'@store': './store',
+					},
+				},
+			],
+		],
+	};
 };
